39476d91e0da13878374fec22ff7c547b476b730
NehemiahLimo <nehemiahlimo02@gmail.com>
2019-09-17 23:44:52 +0300

    Revert "Building API Customers"

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using AutoMapper;
-using Vidly.App_Start;
 
 namespace Vidly
 {
@@ -15,8 +12,6 @@ namespace Vidly
     {
         protected void Application_Start()
         {
-            Mapper.Initialize(c =>c.AddProfile<MappingProfile>());
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
